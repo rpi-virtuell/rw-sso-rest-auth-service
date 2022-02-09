643ebd93186ebcf20fe202722ff7b61ecd9521c6
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2022-02-09 19:51:57 +0100

    INITIAL COMMIT

---
+++ sso-rest-auth-service.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Plugin Name:      RW SSO REST AUTH SERVICE
+ * Plugin URI:       https://github.com/rpi-virtuell/rw-sso-rest-auth-service
+ * Description:      Authentication tool to compare Wordpress login Data with a Remote Login Server
+ * Author:           Daniel Reintanz
+ * Version:          1.0.0
+ * Licence:          GPLv3
+ * GitHub Plugin URI: https://github.com/rpi-virtuell/rw-sso-rest-auth-service
+ * GitHub Branch:     master
+ */
+
+class SsoRestAuthService
+{
+
+    static private $instance = NULL;
+
+    /**
+     * Plugin constructor.
+     *
+     * @since   0.1
+     * @access  public
+     * @uses    plugin_basename
+     * @action  rw_remote_auth_server_init
+     */
+    public function __construct()
+    {
+        add_action('rest_api_init', 'register_SSO_rest_routes');
+        add_action('init', array('SSORESTAuth', 'add_endpoint'), 0);
+        $controller = new SsoRestAuthService();
+        $controller->register_routes();
+    }
+
+    /**
+     * Register the routes for the objects of the controller.
+     */
+    public function register_routes()
+    {
+        $version = '1';
+        $namespace = 'sso-/v' . $version;
+        $base = 'check_credentials';
+        register_rest_route($namespace, '/' . $base, array(
+            array(
+                'methods' => 'POST',
+                'callback' => array($this, 'handle_request'),
+                'args' => array(
+                    'page' => array(
+                        'required' => false
+                    ),
+                    'per_page' => array(
+                        'required' => false
+                    ),
+                ),
+            ),
+        ));
+    }
+
+    public function handle_request(WP_REST_Request $request)
+    {
+
+        $request = $request->get_body();
+        $requestObj = json_decode($request);
+        if (null === $requestObj) {
+            $data = array('auth' => array("success" => false));
+        } else {
+            $user = $requestObj->user->id;
+            $mxid = $user;
+            $user = substr($user, 1, strpos($user, ':') - 1);
+            $password = addslashes($requestObj->user->password);
+
+            $LoginUser = wp_authenticate($user, $password);
+            if (!is_wp_error($LoginUser) && !empty($LoginUser)) {
+                $data = array('auth' => array(
+                    "success" => true,
+                    "mxid" => $mxid,
+                    "profile" => array(
+                        "display_name" => $LoginUser->display_name,
+                    ),
+                ));
+            } else {
+                $data = array('auth' => array("success" => false));
+            }
+        }
+
+        $response = new WP_REST_Response($data);
+
+        $response->set_status(201);
+        return $response;
+    }
+}
+
