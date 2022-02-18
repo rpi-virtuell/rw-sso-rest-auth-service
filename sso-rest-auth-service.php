7b150ac36d20a0ae7c03674a06c73ea4501fc60e
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2022-02-18 14:54:08 +0100

    CHANGES: REWORKED get_remote_user route ADDED traffic tracker to check_credentials route

--- sso-rest-auth-service.php
+++ sso-rest-auth-service.php
@@ -61,6 +61,20 @@ class SsoRestAuthService
                 ),
             )
         );
+
+        register_rest_route($namespace, '/' . 'get_remote_user', array(
+                'methods' => 'POST',
+                'callback' => array($this, 'get_remote_user'),
+                'args' => array(
+                    'page' => array(
+                        'required' => false
+                    ),
+                    'per_page' => array(
+                        'required' => false
+                    ),
+                ),
+            )
+        );
     }
 
     public function check_credentials(WP_REST_Request $request)
@@ -77,6 +91,7 @@ class SsoRestAuthService
         } else {
             $username = $requestObj['username'];
             $password = $requestObj['password'];
+            $origin_url = $requestObj['origin_url'];
 
             $LoginUser = wp_authenticate($username, $password);
             if (is_a($LoginUser, 'WP_User')) {
@@ -90,11 +105,57 @@ class SsoRestAuthService
                         'user_email' => $LoginUser->user_email
                     )
                 );
+                if (!empty(get_user_meta($LoginUser->ID, 'rw_website_urls', true))) {
+                    $website_urls = get_user_meta($LoginUser->ID, 'rw_website_urls', true);
+                } else {
+                    $website_urls = array();
+                }
+                update_user_meta($LoginUser->ID, 'rw_last_visited_url', $origin_url);
+                update_user_meta($LoginUser->ID, 'rw_last_visited_timestamp', wp_date(get_option('date_format')));
+                array_push($website_urls, $origin_url);
+                $website_urls = array_unique($website_urls);
+                update_user_meta($LoginUser->ID, 'rw_website_urls', $website_urls);
             } else {
                 $data = array("success" => false);
             }
         }
 
+        $response = new WP_REST_Response($data);
+
+        $response->set_status(201);
+        return $response;
+    }
+
+    public function get_remote_user(WP_REST_Request $request)
+    {
+        $data = array("success" => false);
+        if (!in_array($this->ipAddress(), ALLOWED_SSO_CLIENTS)) {
+            $response = new WP_REST_Response();
+            $response->set_status(403);
+            return $response;
+        }
+        $requestObj = $request->get_params();
+
+        if (null === $requestObj) {
+            $response = new WP_REST_Response();
+            $response->set_status(406);
+            return $response;
+        } else {
+            $user_login = $requestObj['user_login'];
+            $user = get_user_by('login', $user_login);
+            if (is_a($user, 'WP_User')) {
+                $user = array(
+                    "display_name" => $user->display_name,
+                    'first_name' => $user->first_name,
+                    'last_name' => $user->last_name,
+                    'user_login' => $user->user_login,
+                    'user_email' => $user->user_email,
+                    'avatar' => get_avatar($user->ID));
+            }
+            if ($user) {
+                $data = array("success" => true, "user" => $user);
+            }
+        }
         $response = new WP_REST_Response($data);
 
         $response->set_status(201);
@@ -124,10 +185,9 @@ class SsoRestAuthService
 
                     $userlist[] = array(
                         "display_name" => $user->display_name,
+                        'user_login' => $user->user_login,
                         'first_name' => $user->first_name,
                         'last_name' => $user->last_name,
-                        'user_login' => $user->user_login,
-                        'user_email' => $user->user_email,
                         'avatar' => get_avatar($user->ID));
                 }
             }
